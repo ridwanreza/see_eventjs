e11a633dbdc33a11b06fb32200263c67783974cd
hussein <fathinsatrian358@gmail.com>
2021-11-10 19:03:21 +0700

    project & structure intialized

---
+++ SeeEvent_TeamD/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import {View, Text} from 'react-native';
+
+
+
+ const App = () => {
+  return (
+    <View>
+      <Text>THIS IS APP.JS</Text>
+    </View>
+  );
+}
+
+export default App;
