cc39cdbf7b4e3ba4a4ea33eec15676b199bb644d
ridwanreza <ridwanreza77@gmail.com>
2021-11-12 21:13:09 +0700

    splash screen done

--- SeeEvent_TeamD/src/screens/Index/Idx.js
+++ SeeEvent_TeamD/src/screens/Index/Idx.js
@@ -12,19 +12,29 @@ const Idx = (props) => {
                     source={require('../../assets/splash.png')} 
                     style= {{alignSelf: 'center', marginTop: 100}}
                 />            
-                <View style= {{justifyContent: 'center', alignItems: 'center', flexDirection: 'row', marginTop: 30}}>
-                <Image 
-                    source={require('../../assets/Event.png')} 
-                    style= {{resizeMode: 'stretch', height: 40, width: 56}}
-                />
-                    <Text style= {{fontSize: 32, marginLeft: 12, fontStyle: 'italic',}}>
-                            SeeEvent
+                <View style= {{justifyContent: 'center', 
+                            alignItems: 'center', 
+                            flexDirection: 'row', 
+                            marginTop: 30, 
+                            marginBottom: 20}}>
+                    <Image 
+                        source={require('../../assets/Event.png')} 
+                        style= {{resizeMode: 'stretch', height: 40, width: 56}}
+                    />
+                    <Text style= {{fontSize: 32, 
+                                marginLeft: 12, 
+                                fontStyle: 'italic',}}>
+                        SeeEvent
                     </Text>              
                 </View>
             <Button text= "Sign Up" navigation = {() => props.navigation.navigate('SignUp')}/>
             <Buttonsign text= "Sign In" navigation = {() => props.navigation.navigate('SignIn')}/>            
             <Pressable style= {{alignSelf: 'center'}} onPress={() => props.navigation.navigate('GoToHomeScreen')}> 
-                <Text style= {{textDecorationLine: 'underline', color: '#3E89AE', marginTop: 10}}>Continue as guest</Text>               
+                <Text style= {{textDecorationLine: 'underline', 
+                            color: '#3E89AE', 
+                            marginTop: 10}}>
+                            Continue as guest
+                </Text>               
             </Pressable>
             <Text style= {{alignSelf: 'center', marginTop: 60}}>version v1.1.2</Text>            
             </View>
