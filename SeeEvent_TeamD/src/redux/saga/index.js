4bbeaf296d61222b17224a475e6f50e612d8136f
hussein <fathinsatrian358@gmail.com>
2021-11-19 18:10:32 +0700

    t

--- SeeEvent_TeamD/src/redux/saga/index.js
+++
@@ -1,6 +0,0 @@
-import {all} from 'redux-saga/effects';
-import authSaga from './atuhsaga';
-import homeSaga from './homeSaga';
-export default function* rootSaga() {
-  yield all([authSaga(), homeSaga()]);
-}
