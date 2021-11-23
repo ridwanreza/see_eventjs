088c8c207e55d65b83e5ce40e9b93c4ea6d0f3bd
hussein <fathinsatrian358@gmail.com>
2021-11-23 09:51:57 +0700

    asd

--- SeeEvent_TeamD/src/redux/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import auth from './auth';
-import home from './home';
-import signup from './signup';
-export default combineReducers({
-  auth,
-  home,
-  signup,
-});
